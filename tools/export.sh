922f1ee7fc64020d1801b3fba3522150a7c7539a
Ninetalesgh <camillolukesch@gmail.com>
2025-05-11 23:01:40 +0200

    directories.. .

--- tools/export.sh
+++ tools/export.sh
@@ -26,16 +26,13 @@ exit_early(){
 }
 
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+BIN_DIR="$PROJECT_DIR/bin"
 
-echo "$PROJECT_DIR"
-
-
-EDITOR_DIR="$PROJECT_DIR/editor$BINARY_POSTFIX"
+EDITOR_DIR="$BIN_DIR/editor$BINARY_POSTFIX"
 EXPORT_DIR="$PROJECT_DIR/export"
-TEMPLATE_DIR="$PROJECT_DIR/godot.${PLATFORM}.template_$CONFIG.$ARCH$BINARY_POSTFIX"
+TEMPLATE_DIR="$BIN_DIR/godot.${PLATFORM}.template_$CONFIG.$ARCH$BINARY_POSTFIX"
 
-echo $EDITOR_DIR
-if [ ! -e "$EDITOR_DIR" ]; then
+if [ ! -f "$EDITOR_DIR" ]; then
 	bash build_editor.sh
 	status=$?
 fi
@@ -44,7 +41,7 @@ if [ $status -ne 0 ]; then
 	exit_early
 fi
 
-if [ ! -e "$TEMPLATE_DIR" ]; then
+if [ ! -f "$TEMPLATE_DIR" ]; then
 	bash build_template_$CONFIG.sh
 	status=$?
 fi
