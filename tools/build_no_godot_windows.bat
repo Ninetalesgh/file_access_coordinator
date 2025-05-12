998fab865d0891645861b2576b7c684c907c14cb
Ninetalesgh <camillolukesch@gmail.com>
2025-05-12 16:40:34 +0200

    fedwhjfld

--- tools/build_no_godot_windows.bat
+++ tools/build_no_godot_windows.bat
@@ -3,7 +3,7 @@ IF NOT DEFINED DevEnvDir call shell_win64.bat
 
 set PROJECT_DIR=%~dp0\..
 
-set compiler_options=/I C:\src\include /GR- /EHa- /FC /MT /nologo /volatile:iso /W4 /wd4068 /wd4100 /wd4201 /wd4701 /wd4189 /wd4530 /Ox
+set compiler_options=/I C:\src\include /GR- /EHa- /FC /MT /nologo /volatile:iso /W4 /wd4068 /wd4100 /wd4201 /wd4701 /wd4189 /wd4530 /wd4996 /Ox
 set linker_options=/link /opt:ref /incremental:no /LIBPATH:"C:\src\lib\libssh" "C:\src\lib\libssh\ssh.lib"
 
 pushd %PROJECT_DIR%\export
