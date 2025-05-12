e559e392fcffecfbcbbc725db4f6f045c60691b6
Ninetalesgh <camillolukesch@gmail.com>
2025-05-12 17:46:11 +0200

    maybe cli is done? ish?

--- tools/build_no_godot_windows.bat
+++ tools/build_no_godot_windows.bat
@@ -7,5 +7,5 @@ set compiler_options=/I C:\src\include /GR- /EHa- /FC /MT /nologo /volatile:iso 
 set linker_options=/link /opt:ref /incremental:no /LIBPATH:"C:\src\lib\libssh" "C:\src\lib\libssh\ssh.lib"
 
 pushd %PROJECT_DIR%\export
-cl %PROJECT_DIR%\modules\ssh_native\main.cpp /Fe:"fac_cli.exe" %compiler_options% %linker_options% 
+cl /std:c++17 %PROJECT_DIR%\modules\ssh_native\main.cpp /Fe:"fac_cli.exe" %compiler_options% %linker_options% 
 popd
